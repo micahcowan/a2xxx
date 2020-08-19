151d0376d272fd9ff86d871b6a887f7b353472ca
Micah Cowan <micah@addictivecode.org>
2020-08-19 02:48:40 -0700

    Assign transform fn at screen start.

--- xxx.s
+++ xxx.s
@@ -14,6 +14,8 @@ CpyFlag = $08
 TxtAddr1= $1A
 TxtAddr2= $1C
 
+TransFn = $30
+
 WNDTOP  = $22
 CH      = $24
 CV      = $25
@@ -35,13 +37,13 @@ DrawMsg:LDA #<Message   ; Initialize start of string
         LDA #>Message
         STA StrPtr+1
         LDY #$00
+        JSR XXX
 DrawLp: LDA (StrPtr),Y  ; Load next charactr
         BEQ DrawEnd     ; Done drawing if == NUL byte
-        JSR XXX         ; Replace letters with "X" if open-apple pressed
+        JSR XCall       ; Replace letters with "X" if open-apple pressed
         JSR MyCout      ; Jump to user-hookable/monitor's char-printing routine
         INY
         BNE DrawLp
-
 DrawEnd:JSR Origin      ; Return cursor to 0, 0
         JSR MaybeCopy
         JSR MaybeSwitch
@@ -51,11 +53,22 @@ MyCout: JMP (CSWL)
 
 XXX:
         BIT OpenApple   ; Test if the open apple is pressed
-        BPL XEnd        ;  return if it isn't
-        JSR IsALetter   ; is the current character an uppercase letter?
+        BPL XNo         ;  return if it isn't
+XYes:   LDA #<XTrans    ; Arrange for all uppercase letters to transform to X
+        STA TransFn
+        LDA #>XTrans
+        STA TransFn+1
+        RTS
+XNo:    LDA #<XEnd
+        STA TransFn
+        LDA #>XEnd
+        STA TransFn+1
+XTrans: JSR IsALetter   ; is the current character an uppercase letter?
         BCS XEnd        ;  return if it isn't
         LDA #('X' | $80)
 XEnd:   RTS
+
+XCall:  JMP (TransFn)
 
 IsALetter:              ;; Sets carry flag if A is NOT an uppercase letter
         CMP #Scr_A      ; Is the character in Acc is <'A' or >'Z'
@@ -82,6 +95,9 @@ Switch: LDA Page2On     ; pressed, use page 2
 
 MaybeCopy:              ;; Copy text buf 1 to txt buf 2 if open-apple and
                         ;; we haven't done it already (check CpyFlag)
+        LDA TransFn
+        CMP #<XTrans
+        BNE MCEnd
         BIT CpyFlag
         BPL MCEnd
         BIT OpenApple
@@ -123,6 +139,20 @@ MCCopyRow3:             ; Copy 120 chars (3 rows)
 MCEnd:  RTS
 
 Message: scrcode $0D
+         scrcode $0D
+         scrcode "THIS IS THE SONG THAT NEVER ENDS,"
+         scrcode $0D
+         scrcode "IT JUST GOES ON AND ON MY FRIEND!"
+         scrcode $0D
+         scrcode "SOME PEOPLE"
+         scrcode $0D
+         scrcode "  - STARTED SINGING IT -"
+         scrcode $0D
+         scrcode "NOT KNOWING WHAT IT WAS,"
+         scrcode $0D
+         scrcode "AND THEY'LL CONTINUE SINGING IT FOREVER"
+         scrcode $0D
+         scrcode "    JUST BECAUSE..."
          scrcode $0D
          scrcode "THIS IS THE SONG THAT NEVER ENDS,"
          scrcode $0D
